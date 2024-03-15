e0d5497a642a7c56b1bea887a9c307b3d9725152
lerotsy <rotsyrazafy@gmail.com>
2024-03-15 13:23:21 -0400

    Fix missing stock symbol

--- src/components/Portfolio/Stock/StockDetailsModal.jsx
+++ src/components/Portfolio/Stock/StockDetailsModal.jsx
@@ -8,7 +8,7 @@ const StockDetailsModal = ({ isOpen, onRequestClose, stock }) => {
     <div className={`modal ${isOpen ? 'open' : ''}`} onClick={onRequestClose}>
       <div className="modal-content" onClick={e => e.stopPropagation()}>
         <button className="modal-close-button" onClick={onRequestClose}>&times;</button>
-        <h2>{stock.name}  ({stock.quantity} units)</h2>
+        <h2>{stock.symbol}  ({stock.quantity} units)</h2>
         <div className="stock-details-modal">
           <PerformanceGraph data={stock.graphData} />
           <StockOrder stock={stock} />
